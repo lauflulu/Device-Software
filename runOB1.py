3ffecd727ed216acb34bf6fe30d908d3831b0a62
Aufinger <ga25tur@mytum.de>
2016-04-28 12:30:48 +0200

    entries are linked to the real-time graph now switches and buttons are functional

--- runOB1.py
+++ runOB1.py
@@ -21,28 +21,28 @@ from matplotlib import style
 
 import tkinter as tk
 import numpy as np
-import scipy as sp
+import scipy.signal as spsg
 import time
 from collections import deque
 #from tkinter import ttk
 #from tkinter import messagebox
 #import Elveflow32 as ork
 
-style.use("ggplot")
+style.use('dark_background')
 
 
 
 class MenuBar(tk.Frame):
-    def __init__(self, parent, *args, **kwargs):
+    def __init__(self, parent, friend, *args, **kwargs):
         # inherit from Frame
-        tk.Frame.__init__(self, parent, *args, *kwargs)
+        tk.Frame.__init__(self, parent, friend, *args, *kwargs)
         self.parent = parent
-
+        self.friend = friend
         # initialize child for correct function of close button 'X'
         self.settings_child = None
 
         # create a frame. enables to group widgets
-        self.frame = tk.Frame(self.parent)
+        self.frame = tk.Frame(self)
 
         # create buttons, labels and other widgets
         self.var = tk.IntVar()
@@ -50,24 +50,29 @@ class MenuBar(tk.Frame):
                                          indicatoron=0, command = self.main_toggle_cb,
                                          variable = self.var)
         self.title = tk.Label(self.frame, text='Custom app for Elveflow OB1 pressure controller')
-        self.settings = tk.Button(self.frame, text = 'settings', command = self.settings_open_cb)
+        self.settings = tk.Button(self.frame, text = 'Settings', command = self.settings_open_cb)
+        self.apply = tk.Button(self.frame, text = 'Apply', command = self.apply_cb)
         self.close = tk.Button(self.frame, text = 'close', command = self.close_cb)
         
         # buttons need to be placed by a geometry manager (pack, grid, place, ...)
         self.mainToggle.pack(side = 'left')
         self.settings.pack(side= 'left')
+        self.apply.pack(side= 'left')
         self.title.pack(side = 'left')
         self.close.pack(side = 'left')
         self.frame.pack()
         
+    def apply_cb(self):
+        self.friend.apply_set_cb()
         
     def main_toggle_cb(self):
-        if self.var.get() == 0:
-            print('MAIN TOGGLE: OFF')
-            self.mainToggle.configure(text = 'OFF')
-        elif self.var.get() == 1:
-            print('MAIN TOGGLE: ON')
+        global mainToggle
+        if self.var.get() == 1:
             self.mainToggle.configure(text = 'ON')
+        else:
+            self.mainToggle.configure(text = 'OFF')
+        mainToggle  = self.var.get()
+
     
     def settings_open_cb(self):
         def close_settings(): # reset child variable on close, for correct function of close button 'X'
@@ -99,14 +104,16 @@ class MainPanel(tk.Frame):
         tk.Frame.__init__(self, parent, *args, *kwargs)
         self.parent = parent
         
-        self.frame = tk.Frame(self.parent) 
+        self.frame = tk.Frame(self) 
+        
         
         # header
-        self.labelChannels = tk.Label(self.frame, text = ['channel'])
-        self.labelFunction = tk.Label(self.frame, text = ['function'])
-        self.applySetPressure = tk.Button(self.frame, text = ['Apply'], command = self.apply_set_cb)
+        self.labelChannels = tk.Label(self.frame, text = 'channel')
+        self.labelFunction = tk.Label(self.frame, text = 'function')
+        self.applySetPressure = tk.Label(self.frame, text = 'Pressure')
         self.labelAmplitude = tk.Label(self.frame, text = 'amplitude')
         self.labelPeriod = tk.Label(self.frame, text = 'period')
+        self.labelPhase = tk.Label(self.frame, text = 'phase (°)')
         self.labelGetPressure = tk.Label(self.frame, text = 'pressure (mbar)')
         
         self.labelChannels.grid(row=0, column = 0)
@@ -114,7 +121,8 @@ class MainPanel(tk.Frame):
         self.applySetPressure.grid(row=0, column = 2)
         self.labelAmplitude.grid(row= 0, column = 3)
         self.labelPeriod.grid(row= 0, column = 4)
-        self.labelGetPressure.grid(row=0, column = 5)
+        self.labelPhase.grid(row= 0, column = 5)
+        self.labelGetPressure.grid(row=0, column = 6)
                 
         # initialize variables for buttons
         self.stateVar = {}  
@@ -122,18 +130,19 @@ class MainPanel(tk.Frame):
         self.pressureVar = {}
         self.amplitudeVar = {}
         self.periodVar = {}
+        self.phaseVar = {}
         
         self.pressureSets = []
         self.amplitudeSets = []
         self.periodSets = []
+        self.phaseSets = []
         self.pressureGets = []
         
         self.colours = {'1':'orange','2':'lightgreen','3':'lightblue','0':'yellow'}
-        channels = []
+        
         
         for ch in range(4):
             # initialize channels
-            channels.append(Channel(ch)) 
             channels[ch].colour = self.colours[str(ch)]
 
             # assign variables, set default values
@@ -152,6 +161,9 @@ class MainPanel(tk.Frame):
             self.periodVar[str(ch)] = tk.StringVar()
             self.periodVar[str(ch)].set(channels[ch].period)
             
+            self.phaseVar[str(ch)] = tk.StringVar()
+            self.phaseVar[str(ch)].set(channels[ch].phase)
+            
             self.toggle = tk.Checkbutton(self.frame, text = str(ch), bg = self.colours[str(ch)],
                                         indicatoron=1, command = self.toggle_cb,
                                         variable = self.stateVar[str(ch)])
@@ -164,48 +176,60 @@ class MainPanel(tk.Frame):
             vcmd = (root.register(self.validate),
                 '%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
             # input boxes to enter pressures        
-            self.pressureSet = tk.Entry(self.frame, textvariable = self.pressureVar[str(ch)],
-                                        validate = 'key', validatecommand = vcmd)
-            self.amplitudeSet = tk.Entry(self.frame, textvariable = self.amplitudeVar[str(ch)],
-                                        validate = 'key', validatecommand = vcmd)
-            self.periodSet = tk.Entry(self.frame, textvariable = self.periodVar[str(ch)],
-                                        validate = 'key', validatecommand = vcmd)
+            self.pressureSet = tk.Spinbox(self.frame, textvariable = self.pressureVar[str(ch)],
+                                        validate = 'key', validatecommand = vcmd,
+                                        from_ = 0, to = 2000, increment  = 1)
+            self.amplitudeSet = tk.Spinbox(self.frame, textvariable = self.amplitudeVar[str(ch)],
+                                        validate = 'key', validatecommand = vcmd,
+                                        from_ = 0, to = 2000, increment  = 1)
+            self.periodSet = tk.Spinbox(self.frame, textvariable = self.periodVar[str(ch)],
+                                        validate = 'key', validatecommand = vcmd,
+                                        from_ = 0, to = 2000, increment  = 1)
+            self.phaseSet = tk.Spinbox(self.frame, textvariable = self.phaseVar[str(ch)],
+                                        validate = 'key', validatecommand = vcmd,
+                                        from_ = 0, to = 2000, increment  = 1)
             self.pressureGet = tk.Label(self.frame, text = '0')
             
             self.pressureSets.append(self.pressureSet)
             self.amplitudeSets.append(self.amplitudeSet)
             self.periodSets.append(self.periodSet)
-            self.pressureGets.append(self.pressureGet)  
+            self.pressureGets.append(self.pressureGet) 
+            self.phaseSets.append(self.phaseSet)
                         
             self.toggle.grid(row=ch+1, column = 0)
             self.options.grid(row=ch+1, column = 1)
             self.pressureSet.grid(row=ch+1, column = 2)
             self.amplitudeSet.grid(row=ch+1, column = 3)
             self.periodSet.grid(row=ch+1, column = 4)
-            self.pressureGet.grid(row=ch+1, column = 5)
+            self.phaseSet.grid(row=ch+1, column = 5)
+            self.pressureGet.grid(row=ch+1, column = 6)
             
 
             
         
         self.frame.pack() 
+        #self.apply_set_cb() # this resets channel objects properly ?
         
     def toggle_cb(self):
         for key in self.stateVar:
-            if self.stateVar[key].get() == 0:
-                print('Channel ', key, ': OFF')
-            elif self.stateVar[key].get() == 1:
-                print('Channel ', key, ': ON')
-            print('Function: ', self.optionsVar[key].get())     
+#            if self.stateVar[key].get() == 0:
+#                print('Channel ', key, ': OFF')
+#            elif self.stateVar[key].get() == 1:
+#                print('Channel ', key, ': ON')
+            channels[int(key)].state = self.stateVar[key].get()
 
             
     def apply_set_cb(self):
         for ch in range(len(self.pressureGets)):
             self.pressureGets[ch].configure(text = self.pressureSets[ch].get())
             # write the settings to the channel objects
-            channels[ch].pressure = self.pressureSets[ch].get()
-            channels[ch].amplitude = self.amplitudeSets[ch].get()
-            channels[ch].period = self.periodSets[ch].get()
-            print(ch, channels[ch].pressure, channels[ch].amplitude, channels[ch].period)
+            
+            channels[ch].function = self.optionsVar[str(ch)].get()
+            channels[ch].pressure = float(self.pressureSets[ch].get())
+            channels[ch].amplitude = float(self.amplitudeSets[ch].get())
+            channels[ch].period = float(self.periodSets[ch].get())
+            channels[ch].phase = float(self.phaseSets[ch].get())
+            # print(ch, channels[ch].pressure, channels[ch].amplitude, channels[ch].period)
             # ork.OB1_SetPressure(OB1, int(self.pressureSets[read].get()), ch, 2000)
             # self.pressureReads[ch].configure(text = str(ork.OB1_GetPressure(OB1, ch, 2000)))
             
@@ -225,7 +249,7 @@ class RealTimeGraph(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, *kwargs)
         self.parent = parent
-        self.frame = tk.Frame(self.parent)   
+        self.frame = tk.Frame(self)   
 
         self.canvas = FigureCanvasTkAgg(f, master = self.frame)
 
@@ -237,46 +261,42 @@ class RealTimeGraph(tk.Frame):
         self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
         self.frame.pack() 
         
-    def realtime_data():
-        # all time in seconds
-        period = 30
+    def realtime_data(ch):
         tNow = time.time()-startTime
-        t.append(tNow)
-        t.popleft()
+        t[ch].append(tNow)
+        t[ch].popleft()
         
-        p.append(np.sin((tNow)/period*2*np.pi)+np.random.randn(1)*.05)
-        p.popleft()
+        p[ch].append(int(Channel.wave_pattern(channels[ch]))+np.random.randn(1)*.5)
+        p[ch].popleft()
+            
         
-        return t, p
+        return t[ch], p[ch]
         
-    def realtime_setting():
-        # all time in seconds
-        period = 30
+    def realtime_setting(ch):
         tNow = time.time()-startTime
-        tSet.append(tNow)
-        tSet.popleft()
+        tSet[ch].append(tNow)
+        tSet[ch].popleft()
         
-        pSet.append(np.sin((tNow)/period*2*np.pi))
-        pSet.popleft()
+        pSet[ch].append(int(Channel.wave_pattern(channels[ch])))
+        pSet[ch].popleft()
         
-        return tSet, pSet
+        return tSet[ch], pSet[ch]
 
 class MainApp(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, *kwargs)
+        self.parent = parent
+
+        self.mainPanel = MainPanel(self.parent)
+        self.menuBar = MenuBar(self.parent, self.mainPanel)
+        self.realTimeGraph = RealTimeGraph(self.parent)
         
-        self.menuBar = MenuBar(self)
-        self.mainPanel = MainPanel(self)
-        self.realTimeGraph = RealTimeGraph(self)
-
-
-        self.menuBar.pack(side="bottom", fill="x") 
-        self.mainPanel.pack(side="top", fill="both", expand=True)
-        self.realTimeGraph.pack(side="top", fill="y")
+        self.menuBar.pack(side = 'top')
+        self.mainPanel.pack(side = 'top')
+        self.realTimeGraph.pack(side = 'top')
         
         # change default close behaviour, so device is shut off cleanly
         root.protocol("WM_DELETE_WINDOW", self.close_cb)
-        
         
         
     def close_cb(self):
@@ -286,51 +306,96 @@ class MainApp(tk.Frame):
         
 class Channel():
     def __init__(self, index, state = 0, function = 'const.', pressure = 0, 
-                 amplitude = 0, period = 30, colour = 'red'):
+                 amplitude = 0, period = 30, phase = 0, colour = 'red'):
         self.index = index
         self.state = state
         self.function = function
         self.pressure = pressure
         self.amplitude = amplitude
         self.period = period
+        self.phase = phase
+        self.colour = colour
         
     def wave_pattern(self):
-        if self.state == 1:
-            if self.state == 'const.':
-                p = self.pressure
-            elif self.state == 'sine':
-                tNow = time.time()-startTime
-                p = self.pressure + self.amplitude*np.sin(tNow/self.period*2*np.pi)
-            elif self.state == 'triangle':
-                tNow = time.time()-startTime
-                p = self.pressure + self.amplitude*sp.signal.sawtooth(tNow/self.period*2*np.pi, 0.5)
-            elif self.state == 'square':
-                tNow = time.time()-startTime
-                p = self.pressure + self.amplitude*sp.signal.square(tNow/self.period*2*np.pi, 0.5)
+        pNow = 0
+        if mainToggle == 1:
+            if self.state == 1:
+                if self.function == 'const.':
+                    pNow = self.pressure
+                elif self.function == 'sine':
+                    tNow = time.time()-startTime
+                    pNow = self.pressure + self.amplitude*np.sin(tNow/self.period*2*np.pi-self.phase*np.pi/180.0)
+                elif self.function == 'triangle':
+                    tNow = time.time()-startTime
+                    pNow = self.pressure + self.amplitude*spsg.sawtooth(tNow/self.period*2*np.pi-(self.phase + 270.0)*np.pi/180.0, 0.5)
+                elif self.function == 'square':
+                    tNow = time.time()-startTime
+                    pNow = self.pressure + self.amplitude*spsg.square(tNow/self.period*2*np.pi-self.phase*np.pi/180.0, 0.5)
+                else:
+                    pass
+            else:
+                pass
         else:
-            p = 0
-            
-        return p
+            pass
 
+        return pNow
+        
+    def calculated_pattern(self):
+        pNow = 0
+        input_ = '1=1-2-3'
+        if mainToggle == 1:
+            if self.state == 1:
+                pass
+                # need to work with channel objects in wave pattern?
+                # need attribute with deque?
+                
+
+
+global channels
+channels = []
+global mainToggle
+mainToggle = 0
+
+p = [0,0,0,0]
+t = [0,0,0,0]
+pSet = [0,0,0,0]
+tSet = [0,0,0,0]
+
+for ch in range(4):
+    # initialize channels
+    channels.append(Channel(ch)) 
+    
+    p[ch] = deque(np.empty(60) * np.nan)
+    t[ch] = deque(np.linspace(0, 60, 60))
+
+    pSet[ch] = deque(np.zeros(60))
+    tSet[ch] = deque(np.linspace(0, 60, 60))
 
 f = Figure(figsize=(5, 5), dpi=100)
 a = f.add_subplot(111)
 
+
+
 startTime = time.time()
-t = deque(np.linspace(0, 60, 60))
-p = deque(np.empty(60) * np.nan)
-tSet = deque(np.linspace(0, 60, 60))
-pSet = deque(np.sin((np.linspace(0, 60, 60))/30*2*np.pi))
+
+#t = deque(np.linspace(0, 60, 60))
+#p = deque(np.empty(60) * np.nan)
+
 
 def animate(i): 
-    t, p = RealTimeGraph.realtime_data()
-    tSet, pSet = RealTimeGraph.realtime_setting()
     a.clear()
-    a.plot(t, p)
-    a.plot(tSet, pSet)
+    
+    #tSet, pSet = RealTimeGraph.realtime_setting()
+    for ch in range(4):
+        t, p = RealTimeGraph.realtime_data(ch)
+        a.plot(t, p, channels[ch].colour, marker = '.', linestyle = 'None')
+        tSet, pSet = RealTimeGraph.realtime_setting(ch)
+        a.plot(tSet, pSet, channels[ch].colour)
+    #a.plot(tSet, pSet)
         
 if __name__ == '__main__':
     root = tk.Tk() # create window
+    
     app = MainApp(root).pack(side="top", fill="both", expand=True)
     ani = animation.FuncAnimation(f, animate, interval=1000)
     root.mainloop()
